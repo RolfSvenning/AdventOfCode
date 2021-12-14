76d1ea699649d0f738d30a011f3300fc0746c57e
Rolf <au591328@post.au.dk>
2021-12-14 23:43:07 +0100

    day14

--- 2021/Solutions/14.py
+++ 2021/Solutions/14.py
@@ -1,5 +1,4 @@
-import numpy as np
-
+import collections
 
 def part_one_and_two():
     polymer, rules = open("2021/input/14.txt").read().split("\n\n")
@@ -8,30 +7,44 @@ def part_one_and_two():
     for l,r in rules:
         rulesMap[l] = r
     
-    insertions = []
-    for i, (a,b) in enumerate(zip(polymer,polymer[1:])):
-        print("ab: ", a + b)
-        if (ab := a + b) in rulesMap:
-    #         print("TRUE")
-    #         print(rulesMap[ab])
-            insertions.append((i, rulesMap[ab]))
-    print(insertions)
+    countsPairs = collections.defaultdict(int)
+    counts = collections.defaultdict(int)    
 
+    for a,b in zip(polymer,polymer[1:]):
+        countsPairs[a + b] += 1
+        counts[a] += 1
+    counts[polymer[-1]] += 1
+    
+    for i in range(40):
+        nextCount = collections.defaultdict(int)
+        for a,b in [(k[0], k[1]) for k in countsPairs.keys()]:
+            nextCount[a + rulesMap[a + b]] += max(countsPairs[a + b], 1)
+            nextCount[rulesMap[a + b] + b] += max(countsPairs[a + b], 1)
+            counts[rulesMap[a + b]] += max(countsPairs[a + b], 1)
+        countsPairs = nextCount.copy()
+        if i == 9:
+            print("Part one, difference between most and least common: ", max(counts.values()) - min(counts.values()))
+    print("Part two, difference between most and least common: ", max(counts.values()) - min(counts.values()))
 
-    currIndex = 0
-    curr = insertions[0][0]
-    res = []
-    print("curr: ", curr)
-    for i in range(len(polymer)):
-        while i <= curr:
-            res.append[polymer[i]]
-            if i == curr:
-                break
-        curr = 
+# def naive_part_one():
+#     polymer, rules = open("2021/input/14.txt").read().split("\n\n")
+#     rules = [x.split(" -> ") for x in rules.split("\n")]
+#     rulesMap = {}
+#     for l,r in rules:
+#         rulesMap[l] = r
+    
+#     for _ in range(10):
+#         currPolymer = ""
+#         for a,b in zip(polymer,polymer[1:]):
+#             currPolymer += a + rulesMap[a + b]
+#         currPolymer += polymer[-1]
+#         polymer = currPolymer
 
-
-        
-
-
+#     C = collections.Counter(polymer)
+#     print(polymer)
+#     print("Part one, difference in frequence between most common and least common element in final string is: ", 
+#            C.most_common()[0][1] - C.most_common()[-1][1])
+           
 if __name__ == '__main__':
     part_one_and_two()
+    # naive_part_one()
