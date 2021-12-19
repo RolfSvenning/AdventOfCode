914c9eae9f8ef996965de9e6138d1ca8c711f8c1
Rolf <201708195@post.au.dk>
2021-12-19 01:10:16 +0100

    day18

--- 2021/Solutions/18.py
+++ 2021/Solutions/18.py
@@ -2,54 +2,49 @@ import re
 from numpy import floor, ceil
 
 def part_one_and_two():
-    # i = re.split("[ ,.=]+", open("2021/input/17.txt").readline().strip())
     lists =  [l.strip() for l in  open("2021/input/18.txt").readlines()]
-    
-    for i in range(len(lists) - 1):
-        l = "[" + lists[0] + "," + lists[1] + "]"
-        l = explode(l)
-        break
+    n = len(lists)
+    for i in range(n - 1):
+        lists[0] = explode(pairLists(lists[0], lists[i]))
+    print("Part one,sum is: ", lSum(eval(lists[0])))
 
-    # print("RESULT: ", lists)
+    bestSum = 0
+    for i,j in [(i,j) for i in range(n) for j in range(n)]:
+        bestSum = max(bestSum, lSum(eval(explode(pairLists(lists[i], lists[j])))))
+    print("Part two, sum of best pair: ", bestSum)
 
-def explode(l):
-    print("exploding:","\n" + l)
+def pairLists(l1,l2):
+    return "[" + l1 + "," + l2 + "]"
+
+def explode(l): # mutually recursive with split
     depth = 0
     for i in range(len(l)):
-        s = l[i]
         assert depth <= 5
-        match s:
+        match s := l[i]:
             case "[": depth += 1
             case "]": depth -= 1
             case ",": continue
             case _: 
                 if depth < 5: continue
-                assert re.compile('[0-9]').match(s) != None
                 j = i + 1
                 while l[j] != "]":
                     j += 1
-                    # print(l[j])
-                # print("i,j: ", i,j)
                 ss = l[i:j]
-                a,b = ss.split(",")
-                assert re.compile('[0-9]+,[0-9]+').match(ss) != None # check that pair does not contain another pair
-                if (r := re.compile('[0-9]+').search(l[0:i][::-1])) != None: #looking for digits to the left
-                    # print(r[0], r.span())
-                    # print(l[:i-r.span()[1]])
-                    new  = str(int(a) + int(r[0]))
+                a,b = ss.split(",") #exploding pair a,b
+                # looking for digits to the left
+                if (r := re.compile('[0-9]+').search(l[0:i][::-1])) != None:
+                    new  = str(int(a) + int(r[0][::-1]))
                     ll = l[:i-r.span()[1]] + new + l[i - r.span()[0]: i - 1]
                 else:
-                    ll = l[0:i-1]y
-                if (r := re.compile('[0-9]+').search(l[j:])) != None: #looking for digits to the right
+                    ll = l[0:i-1]
+                # looking for digits to the right
+                if (r := re.compile('[0-9]+').search(l[j:])) != None: 
                     new = str(int(b) + int(r[0]))
-                    lr = new + l[j + r.span()[1]:]
+                    lr = l[j + 1:j + r.span()[0]] + new + l[j + r.span()[1]:] # skip first [, then then take [,s to update then rest
                 else:
-                    lr = "0" + l[j:]
-
-                print(ll + "--" + lr)
-                l = ll + "," + lr
-                print(l)
-                break # exploded
+                    lr = l[j + 1:]
+                l = ll + "0" + lr
+                break
     else:
         return split(l)
     return explode(l)
@@ -58,15 +53,16 @@ def explode(l):
 def split(l):
     r = re.compile('[0-9][0-9]+').search(l)
     if r != None:
-        print(int(r[0]))
-        new = "[" + str(int(floor(int(r[0]) / 2))) + "," + str(int(ceil(int(r[0]) / 2))) + "]"
-
-        lNew = l[:r.span()[0]]  + new + l[r.span()[1]:]
-        print(lNew)
-        return explode(lNew)
+        new = "[" + str(int(floor(int(r[0]) / 2))) + "," + str(int(ceil(int(r[0]) / 2))) + "]" # 13 -> [6,7]
+        return explode(l[:r.span()[0]]  + new + l[r.span()[1]:])
     else:
         return l
 
+def lSum(l):
+    if type(l) is int:
+        return l
+    else:
+        return 3 * lSum(l[0]) + 2 * lSum(l[1])
 
 
 if __name__ == '__main__':
