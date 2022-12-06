bc2c974e59f073e8913ad346983fe84991faae8c
RolfSvenning <rolfsvenning@gmail.com>
2022-12-06 09:34:39 +0100

    06

--- 2022/Solutions/06.py
+++ 2022/Solutions/06.py
@@ -1,9 +1,11 @@
 signal = open("2022/Input/06.txt").read().strip()
 
+### <---------------- PART ONE ----------------> ###
 for i in range(0, len(signal) - 3):
     if len(set(signal[i:i + 4])) >= 4: break
 print(i + 4)
 
+### <---------------- PART TWO ----------------> ###
 for i in range(i, len(signal) - 13):
     if len(set(signal[i:i + 14])) >= 14: break
 print(i + 14)
