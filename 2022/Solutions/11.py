e37aaebb43018098771cc2ab0a20eb9da98b9c76
Rolf <au591328@post.au.dk>
2022-12-11 13:03:15 +0100

    11

--- 2022/Solutions/11.py
+++ 2022/Solutions/11.py
@@ -1,11 +1,12 @@
 import re
 import copy
+from functools import reduce
 
 monkeys = open("2022/Input/11.txt").read().strip().split("Monkey ")[1:]
-n = len(monkeys)
-print("n", n)
 monkeys = [[re.findall("\d+|old|[+]|[*]", l) for l in m.strip().split("\n")] for m in monkeys]
+n = len(monkeys)
 
+### -----------------------> PART ONE -----------------------> ###
 class Monkey():
     def __init__(self, id, items, op, test, monkeyTrue, monkeyFalse):
         self.id = int(id[0])
@@ -18,27 +19,19 @@ class Monkey():
         self.partOne = True
         self.divis = int(test[0])
 
-    def __str__(self):
-        return f"Monkey {self.id}: Starting items: {str(self.items)[1:-1]}"
-
     def addItem(self, item):
         if self.partOne: self.items.append(item)
         else: self.items += [item]
 
-
     def throwItems(self):
-        self.inspects += len(self.items) #96577
+        self.inspects += len(self.items)
         for item in self.items:
             if self.partOne: 
                 newItem = self.op(item) // 3
                 worryToTest = newItem
             else: 
-                # print(divisors)
-                # print(self.items)
-                # print(item)
                 newItem = [self.op(item[i]) % self.divisors[i] for i in range(n)]
                 worryToTest = newItem[self.id]
-
             if self.test(worryToTest):   self.monkeyTrue.addItem(newItem)
             else:                        self.monkeyFalse.addItem(newItem)
         self.items = []
@@ -46,44 +39,27 @@ class Monkey():
     def changeToPartTwo(self, divisors): 
         self.partOne = False
         self.divisors = divisors
-        # print(self.items)
-        # print([[item]*4 for item in self.items])
         self.items = [[item] * n for item in self.items] # make a version of the item for each monkey
         
-
 monkeys = [Monkey(*m) for m in monkeys]
 for m in monkeys:
     m.monkeyFalse = monkeys[m.monkeyFalse]
     m.monkeyTrue = monkeys[m.monkeyTrue]
 monkeys2 = copy.deepcopy(monkeys)
-monkeys3 = copy.deepcopy(monkeys)
 
-for r in range(20): # rounds
+for _ in range(20): # rounds
     for m in monkeys:
         m.throwItems()
 
-print("PART ONE:", sorted([m.inspects for m in monkeys])[-1] * sorted([m.inspects for m in monkeys])[-2])
+print("PART ONE:", reduce(mul := lambda x,y: x*y, sorted([m.inspects for m in monkeys])[-2:]))
 
-naive = [100000000000000000000000000000000000000000000] * n
-print([m.divis for m in monkeys2])
+### -----------------------> PART TWO -----------------------> ###
 divisors = [m.divis for m in monkeys2]
-print("maxDivis", divisors)
-def test(monkeys, divisors):
-    for m in monkeys:
-            m.changeToPartTwo(divisors)
-    for _ in range(10000): # rounds
-        for m in monkeys:
-            m.throwItems()
+for m in monkeys2:
+        m.changeToPartTwo(divisors)
 
-test(monkeys2, naive)
-test(monkeys3, divisors)
+for _ in range(10000): # rounds
+    for m in monkeys2:
+        m.throwItems()
 
-
-
-print([m.inspects for m in monkeys2])
-# print([m.items for m in monkeys2])
-print([m.inspects for m in monkeys3])
-# print([m.items for m in monkeys3])
-
-print("PART TWO:", sorted([m.inspects for m in monkeys3])[-1] * sorted([m.inspects for m in monkeys3])[-2])
-
+print("PART TWO:", reduce(mul, sorted([m.inspects for m in monkeys2])[-2:]))
