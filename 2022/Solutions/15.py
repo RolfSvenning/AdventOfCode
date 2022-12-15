2b2d4fde47fdbeaff3072809ab1fedb81101bf32
RolfSvenning <rolfsvenning@gmail.com>
2022-12-15 14:57:15 +0100

    15

---
+++ 2022/Solutions/15.py
@@ -0,0 +1,110 @@
+import re
+from functools import cmp_to_key
+from collections import defaultdict
+
+
+input = [[int(x) for x in re.findall("-*\d+", l)] for l in open("2022/Input/15.txt").readlines()]
+ys = [x for s in input for x in [s[1], s[3]]]
+n, ymin, ymax = len(input), min(ys), max(ys)
+yrange = ymax - ymin + 1
+
+def manhattenDist(l):
+    sx, sy, bx, by = l
+    return abs(sx - bx) + abs(sy - by)
+
+def returnStartAndEnd(sx, d):
+    return [("s", sx - d), ("e", sx + d)]
+
+l = [[] for _ in range(-yrange, 2*yrange)] # input wraps around!
+for i in range(n):
+    print(i)
+    d = manhattenDist(input[i])
+    sx, sy, bx, by = input[i]
+
+    l[sy] += returnStartAndEnd(sx, d)
+    for j in range(1, d + 1):
+        l[sy - j] += returnStartAndEnd(sx, d - j)
+        l[sy + j] += returnStartAndEnd(sx, d - j)
+
+def orderOfEventPoints(e1, e2):
+    t1, x1 = e1
+    _,  x2 = e2
+    if x1 == x2: 
+        if t1 == "s":   return -1
+        else:           return  1
+    else:
+        if x1 < x2:     return -1
+        else:           return  1
+
+
+B = defaultdict(int)
+for bx, by in set([(bx, by) for _, _, bx, by in input]):
+    B[by] += 1
+
+
+def calcRow(i, rowEventPoints):
+    if len(rowEventPoints) == 0: return 0
+    rowEventPoints = sorted(rowEventPoints, key=cmp_to_key(lambda e1, e2: orderOfEventPoints(e1, e2)))
+    covered = 1
+    numberOfOpenIntervals = 0
+    _, last = rowEventPoints[0]
+    for t,x in rowEventPoints:
+        if numberOfOpenIntervals > 0:
+                    covered += x - last
+        if numberOfOpenIntervals == 0 and x != last and t == "s": 
+            covered += 1
+        last = x
+        # print(covered, x)
+        match t: 
+            case "s": numberOfOpenIntervals += 1
+            case "e": numberOfOpenIntervals -= 1
+            case   _: raise NotImplementedError("should ever happen")
+    return covered - B[i] # subtract beacons
+
+r1 = 10
+# r1 = 2000000
+print("PART ONE:", calcRow(r1, l[r1]))
+
+
+def fixRange(x, xmax):
+    if x < 0:    return 0
+    if x > xmax: return xmax
+    return x
+
+def calcRowPartTwo(i, rowEventPoints, xmax, findIndex=False):
+    assert len(rowEventPoints) > 0
+
+    rowEventPoints = sorted(rowEventPoints, key=cmp_to_key(lambda e1, e2: orderOfEventPoints(e1, e2)))
+    rowEventPoints = [(t, fixRange(x, xmax)) for (t,x) in rowEventPoints]
+    covered = 1
+    numberOfOpenIntervals = 0
+    _, last = rowEventPoints[0]
+    for t,x in rowEventPoints:
+        if numberOfOpenIntervals > 0:
+                    covered += x - last
+        if numberOfOpenIntervals == 0 and x != last and t == "s": 
+            covered += 1
+        last = x
+        # print(covered, x)
+        match t: 
+            case "s": numberOfOpenIntervals += 1
+            case "e": 
+                numberOfOpenIntervals -= 1
+                if numberOfOpenIntervals == 0 and findIndex:
+                    return x + 1
+            case   _: raise NotImplementedError("should ever happen")
+    return covered - B[i] # subtract beacons
+
+
+xmax = 20
+# xmax = 4000000
+for i in range(0, min(len(l), xmax + 1)):
+    if (r := calcRowPartTwo(i, l[i], xmax)) + B[i] != xmax + 1: 
+        print("PART TWO i:", i)
+        print(calcRowPartTwo(i, l[i], xmax, findIndex=True))
+        break
+
+
+
+
+
