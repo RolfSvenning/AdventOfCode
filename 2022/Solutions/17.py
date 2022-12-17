f3712b22a80e612d2cbb032b2f4bd1eccae3bc28
Rolf <au591328@post.au.dk>
2022-12-17 17:49:17 +0100

    17

--- 2022/Solutions/17.py
+++ 2022/Solutions/17.py
@@ -25,29 +25,29 @@ s4 = np.array([[1,1],
 shapes = [s0, s1, s2, s3, s4]
 
 
-def printBoardAndShape(B, s, sX, sY):
-    B_ = B.copy()
-    n,m = np.shape(s)
-    B_[sX - n : sX, sY: sY + m] -= s
-    print(B_, "\n")
+# def printBoardAndShape(B, s, sX, sY):
+#     B_ = B.copy()
+#     n,m = np.shape(s)
+#     B_[sX - n : sX, sY: sY + m] -= s
+#     print(B_, "\n")
 
 
 def simulate(rocksCount, partTwo=False):
     foundCycle = not partTwo
     B = np.array([[1,0,0,0,0,0,0,0,1]] * 7 + [[1] * 9])
-    rockPos = np.shape(B)[0] - 1
-    sX = np.shape(B)[0] - 1 - 3
-    sY = 3
     D = {"<": -1, ">": 1}
     H = {} # heights at time i
-    jetsCount = len(jets)
-    states = {} # state -> buildings placed
+    S = {} # state -> # buildings placed
+
+    rockPos = np.shape(B)[0] - 1
+    sX = rockPos - 3
+    sY = 3
 
     i = 0
     j = -1
     while True:
         j += 1
-        jet = jets[j % jetsCount]
+        jet = jets[j % len(jets)]
         s = shapes[i % 5]
         n, m = np.shape(s)
 
@@ -59,7 +59,7 @@ def simulate(rocksCount, partTwo=False):
         # MOVE DOWN
         sX += 1
         if ((B[sX - n : sX, sY: sY + m] + s) > 1).any():
-            # MAKE MOVE
+            # PLACE SHAPE
             sX -= 1
             B[sX - n : sX, sY: sY + m] += s
             i += 1
@@ -71,17 +71,18 @@ def simulate(rocksCount, partTwo=False):
             sY = 3
 
             # CYCLE DETECTION FOR PART TWO
-            H[i] = np.shape(B)[0] - 7 - 1
-            # State should technically be BFS from top of board to get entire boundary... but this is good enough
-            state = tuple([np.where(B[:,index] == 1)[0][0] for index in range(1, 1 + 7)] + [i % 5, j % jetsCount])
-            if state in states and not foundCycle:
-                last_i = states[state]
-                cycleLen = (i - last_i)
-                cycleCount = ((rocksCount - i) // cycleLen)
-                cyclesHeight = cycleCount * (H[i] - H[last_i])
-                i += cycleLen * cycleCount
-                foundCycle = True
-            states[state] = i
+            if not foundCycle:
+                H[i] = np.shape(B)[0] - 7 - 1
+                # State should technically be BFS from top of board to get entire boundary... but this is good enough
+                state = tuple([np.where(B[:,index] == 1)[0][0] for index in range(1, 1 + 7)] + [i % 5, j % len(jets)])
+                if state in S:
+                    last_i = S[state]
+                    cycleLen = i - last_i
+                    cycleCount = (rocksCount - i) // cycleLen
+                    cyclesHeight = cycleCount * (H[i] - H[last_i])
+                    i += cycleLen * cycleCount
+                    foundCycle = True
+                S[state] = i
 
             if i == rocksCount: break
 
