333b44752f462bb0850ec3581033ae2a687ce8bc
Rolf <au591328@post.au.dk>
2022-12-18 11:32:36 +0100

    18 1/2

---
+++ 2022/Solutions/18.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+input = [np.array([int(x) for x in l.strip().split(",")]) for l in open("2022/Input/18.txt").readlines()]
+n = len(input)
+print("n", n)
+
+
+def absD(p,q): 
+    return np.sum(np.absolute(p - q))
+
+def partOne():
+    surfaceArea = 6 * n
+    for a in input:
+        for b in input:
+            if absD(a, b) == 1: surfaceArea -= 1
+            
+    print("PART ONE:", surfaceArea) 
+    return surfaceArea
+
+
+surfaceArea = partOne()
+# surfaceArea = 4536
+ 
+
+
+setInput = set(tuple(input[i]) for i in range(len(input)))
+print("n", len(setInput))
+
+def trapped(p):
+    for i in range(3):
+        for delta in [-1, 1]:
+            p_ = p[:]
+            p_[i] += delta
+            if tuple(p_) not in setInput:
+                return 0
+    return 1
+
+
+print("trapped [2 2 5]:" , trapped([2,2,5]))
+
+countTrapped = 0
+for p in input:
+    for i in range(3):
+        for delta in [-1, 1]:
+            p_ = p[:]
+            p_[i] += delta
+            if tuple(p_) not in setInput and trapped(p_.tolist()): 
+                countTrapped += 1
+
+print("countTrapped:", countTrapped / 3)
+print("surfaceArea:", surfaceArea)
+print("PART TWO:", surfaceArea - ((countTrapped / 3) * 6))
