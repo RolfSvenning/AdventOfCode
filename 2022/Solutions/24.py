14f670e9f6b0f2f102424e2844d1e84dc3b508c9
RolfSvenning <rolfsvenning@gmail.com>
2022-12-24 14:51:36 +0100

    24

--- 2022/Solutions/24.py
+++ 2022/Solutions/24.py
@@ -15,29 +15,19 @@ def updateState(S):
             case "^": return ((x - 1) % n, y, d)
     return set([updateBlizzard(b) for b in S])
 
-
-states = [S]
+LS = [S]
 for i in range(n * m):
     S = updateState(S)
-    if S in states: break
-    states.append(S)
-states = [[(x,y) for x,y,_ in states[i]] for i in range(len(states))]
-
-
-def printState(S):
-    ls = [["."] * m for _ in range(n)]
-    for x,y,d in S:
-        ls[x][y] = d
-    print("".join(["".join(l) + "\n" for l in ls]))
-
+    if S in LS: break
+    LS.append(S)
+LS = [[(x,y) for x,y,_ in LS[i]] for i in range(len(LS))]
 
 def adjMoves(p_s):
     p,s = p_s
     x,y = p
-    currP = [(p, s + 1)] if p not in states[((s + 1) % len(states))] else []
+    currP = [(p, s + 1)] if p not in LS[((s + 1) % len(LS))] else []
     return currP + [(p_, s + 1) for p_ in [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)] 
-                    if p_ not in states[(s + 1) % len(states)] and 0 <= p_[0] < n and 0 <= p_[1] < m]
-
+                    if p_ not in LS[(s + 1) % len(LS)] and 0 <= p_[0] < n and 0 <= p_[1] < m]
 
 def BFS(start_time, end):
     Q = [start_time]
@@ -45,14 +35,13 @@ def BFS(start_time, end):
     for p, ps in Q:
         if p == end: break
         for q, qs in adjMoves((p, ps)):
-            if (q, qs % len(states)) in visited: continue
-            visited.add((q, qs % len(states)))
+            if (q, qs % len(LS)) in visited: continue
+            visited.add((q, qs % len(LS)))
             Q.append((q, qs))
-    return (ps + 1)
-
+    return ps + 1
 
 t1 = BFS((start, 0), endGoal)
 print("PART ONE:", t1)
 t2 = BFS((end, t1 + 1), startGoal)
 t3 = BFS((start, t2 + 1), endGoal)
-print(t1, t2, t3, "\nPART TWO:", t3)
+print("PART TWO:", t3)
