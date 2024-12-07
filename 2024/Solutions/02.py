6d1d08830dc76e46b9f3b7b28d5e50d17efdb4f0
Rolf <au591328@post.au.dk>
2024-12-07 19:01:50 +0100

    02, 03

--- 2024/Solutions/02.py
+++ 2024/Solutions/02.py
@@ -1,1 +1,21 @@
-# A, B = list(zip(*[list(map(int, l.strip().split())) for l in open("2024/input/01.txt").readlines()]))
+R = list(list(map(int, l.split(" "))) for l in open("2024/input/02.txt").readlines())
+
+def isSafe(r): 
+    if not (r == sorted(r) or r == sorted(r)[::-1]):
+        return False
+    for i in range(len(r)):
+        if i != 0:
+            if not abs(r[i - 1] - r[i]) in [1, 2, 3]: return False
+        if i != len(r) - 1:
+            if not abs(r[i + 1] - r[i]) in [1, 2, 3]: return False
+    return True     
+
+
+print("PART ONE: ", sum(1 for r in R if isSafe(r))) 
+
+def isSafe2(r): 
+    for i in range(len(r) + 1):
+        if isSafe(r[:i - 1] + r[i:]): return True
+    return False
+
+print("PART TWO: ", sum(1 for r in R if isSafe2(r))) 
---
+++ 2024/Solutions/03.py
@@ -0,0 +1,13 @@
+
+import re
+
+def f(s):
+    I = list([list(map(int, re.findall(r'\d+', l))) for l in re.findall(r'mul\(\d{1,3},\d{1,3}\)', s)])
+    return sum(a * b for a, b in I)
+
+
+print("PART ONE: ", f(open("2024/input/03.txt").read())) 
+
+DOs = [s.split("don't()")[0] for s in open("2024/input/03.txt").read().split("do()")]
+
+print("PART TWO: ", sum(f(s) for s in DOs)) 
